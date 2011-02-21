ee7669496b983389fcc6482403fe0d6e64f2f8e2
Jonathan Rockway <jon@jrock.us>
2011-02-21 06:30:59 -0600

    make the repl autostart on port 4343

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,2 @@
-pref("toolkit.defaultChromeURI", "chrome://mozrepl-standalone/chrome/main.xul");
+pref("toolkit.defaultChromeURI", "chrome://mozrepl-standalone/content/main.xul");
+pref("browser.dom.window.dump.enabled", true);
