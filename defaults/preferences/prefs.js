45ac7f399795d155bd79dce24658036d2bdd372b
Jonathan Rockway <jon@jrock.us>
2011-02-21 22:27:23 -0600

    defaults for debugging

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,2 +1,6 @@
 pref("toolkit.defaultChromeURI", "chrome://mozrepl-standalone/content/main.xul");
 pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
